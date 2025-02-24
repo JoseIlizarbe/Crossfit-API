4ba99ffd393b8aa008ec74cd3bc371d96eae87e1
joseilizarbe <jose.ilizarbee@gmail.com>
2025-02-24 18:55:31 -0500

    Change: workoutRoutes

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const workoutController = require("../../controllers/workoutController");
 
 router
-    .get ("/", (req,res) =>{
-        res.send('Obtener todos los WorkOuts');
-    })
-    .get ("/:workoutId", (req,res) =>{
-        res.send(`Obtener WorkOut ${req.params.workoutId}`);
-    })
-    .post ("/:workoutId", (req,res) =>{
-        res.send(`Crear WorkOut ${req.params.workoutId}`);
-    })
-    .patch ("/:workoutId", (req,res) =>{
-        res.send(`Editar WorkOut ${req.params.workoutId}`);
-    })
-    .delete ("/:workoutId", (req,res) =>{
-        res.send(`Eliminar WorkOut ${req.params.workoutId}`);
-    })
+    .get ("/", workoutController.getAllWork)
+    .get ("/:workoutId",workoutController.getOneWorkout)
+    .post ("/:workoutId", workoutController.createNewWorkout)
+    .patch ("/:workoutId", workoutController.updateOneWorkout)
+    .delete ("/:workoutId", workoutController.deleteOneWorkout);
+
+module.exports = router;
