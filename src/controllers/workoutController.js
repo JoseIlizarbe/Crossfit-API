936e52940b3e45204fcaf6cab8961b2f75161fb4
joseilizarbe <jose.ilizarbee@gmail.com>
2025-02-24 19:08:35 -0500

    Fix: get workoutRoute

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,16 +1,16 @@
-const getAllWorkouts = (res, res) => {
+const getAllWorkouts = (req, res) => {
     res.send('Obtener todos los WorkOuts');
 }
-const getOneWorkout = (res, res) => {
+const getOneWorkout = (req, res) => {
     res.send(`Obtener WorkOut ${req.params.workoutId}`);
 }
-const createNewWorkout = (res, res) => {
+const createNewWorkout = (req, res) => {
     res.send(`Crear WorkOut ${req.params.workoutId}`);
 }
-const updateOneWorkout = (res, res) => {
+const updateOneWorkout = (req, res) => {
     res.send(`Editar WorkOut ${req.params.workoutId}`);
 }
-const deleteOneWorkout = (res, res) => {
+const deleteOneWorkout = (req, res) => {
     res.send(`Eliminar WorkOut ${req.params.workoutId}`);
 }
 module.exports = {
