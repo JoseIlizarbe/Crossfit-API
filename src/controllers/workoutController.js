190add1007c16430c9ff46ff798e60e215db84d1
joseilizarbe <jose.ilizarbee@gmail.com>
2025-02-25 12:27:08 -0500

    Feat: DB Json/Workout model/Utils

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,16 +1,44 @@
+const workoutService = require("../services/workoutService");
+
 const getAllWorkouts = (req, res) => {
-    res.send('Obtener todos los WorkOuts');
+    const allWorkouts = workoutService.getAllWorkouts();
+    res.send({status: "OK", data: allWorkouts});
 }
+
 const getOneWorkout = (req, res) => {
+    const Workout = workoutService.getOneWorkout(req.params.workoutId);
     res.send(`Obtener WorkOut ${req.params.workoutId}`);
 }
 const createNewWorkout = (req, res) => {
-    res.send(`Crear WorkOut ${req.params.workoutId}`);
+    const  {body}= req;
+    if(
+        !body.name ||
+        !body.mode ||
+        !body.equipment ||
+        !body.exercises || 
+        !body.trainerTips 
+    ){
+        return ;
+    }
+    
+    const newWorkout = {
+        name: body.name,
+        mode: body.mode,
+        equipment: body.equipment,
+        exercises: body.exercises,
+        trainerTips: body.trainerTips
+    };
+
+    const createdWorkout = workoutService.createNewWorkout(newWorkout);
+    res.status(201).send({status: "OK", data: createWorkout});
 }
+
 const updateOneWorkout = (req, res) => {
+    const updateWorkout = workoutService.updateOneWorkout(req.params.workoutId);
     res.send(`Editar WorkOut ${req.params.workoutId}`);
 }
 const deleteOneWorkout = (req, res) => {
+    workoutService.deleteOneWorkout(req.params.workoutId);
     res.send(`Eliminar WorkOut ${req.params.workoutId}`);
 }
 module.exports = {
